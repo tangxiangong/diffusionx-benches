9115529e663b42bf730a7b04e7bd654af4c5351c
tangxiangong <tangxiangong@gmail.com>
2025-05-23 15:46:37 +0800

    feat: add benchmarking for random number sampling in Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,66 @@
+use diffusionx::random::normal;
+use diffusionx::random::{stable, uniform};
+use std::env;
 use std::time;
-use diffusionx::random::stable::sym_standard_rands;
+
+fn timeit<F>(func: F, bench_size: usize) -> Vec<f64>
+where
+    F: Fn() -> i32,
+{
+    (0..bench_size)
+        .map(|_| {
+            let start_time = time::Instant::now();
+            let _ = func();
+            start_time.elapsed().as_secs_f64()
+        })
+        .collect()
+}
+
+fn show_timeit(result: Vec<f64>) {
+    let mean = result.iter().sum::<f64>() / result.len() as f64;
+    let stddev = result.iter().map(|x| (x - mean).powi(2)).sum::<f64>() / result.len() as f64;
+    let min = *result
+        .iter()
+        .reduce(|a, b| if *a < *b { a } else { b })
+        .unwrap();
+    let max = *result
+        .iter()
+        .reduce(|a, b| if *a > *b { a } else { b })
+        .unwrap();
+    println!("mean: {mean:.4}, stddev: {stddev:.4}, min: {min:.4}, max: {max:.4}");
+    println!();
+}
 
 fn main() {
-    let start_time = time::Instant::now();
-    let _rnds = sym_standard_rands(0.7, 100_000_000).unwrap();
-    let elapsed = start_time.elapsed().as_secs_f64();
-    println!("Time taken by Rust: {elapsed} s");
+    let args: Vec<String> = env::args().collect();
+    let bench_size = args.get(1).map(|s| s.parse().unwrap_or(20)).unwrap_or(20);
+
+    let len = 10_000_000;
+
+    println!("=========================Rust==========================");
+    println!();
+
+    let uniform = || {
+        let _rnds = uniform::range_rands(0.0..1.0, len).unwrap();
+        0
+    };
+    println!("------------uniform random number sampling------------");
+    show_timeit(timeit(uniform, bench_size));
+
+    let normal = || {
+        let _rands = normal::standard_rands(len);
+        0
+    };
+    println!("------------normal random number sampling------------");
+    show_timeit(timeit(normal, bench_size));
+
+    let stable = || {
+        let _rnds = stable::sym_standard_rands(0.7, len).unwrap();
+        0
+    };
+    println!("------------stable random number sampling------------");
+    show_timeit(timeit(stable, bench_size));
+
+    println!("=======================================================");
+    println!();
 }
