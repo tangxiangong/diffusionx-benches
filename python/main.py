457c47c446e2b4c1f1a0f4b91701a48b3d4a47c8
tangxiangong <tangxiangong@gmail.com>
2025-05-23 09:15:04 +0800

    fix: reduce sample size in C++, Julia, Python, and Rust implementations

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ import diffusionx
 def main():
     stable = levy_stable(alpha=0.7, beta = 0.0, loc = 0.0, scale = 1.0)
     start_time = time.time()
-    _ = stable.rvs(size=100_000_000)
+    _ = stable.rvs(size=10_000_000)
     elapsed = time.time() - start_time
     print(f"Time taken by Python: {elapsed} s")
 
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ use diffusionx::random::stable::sym_standard_rands;
 
 fn main() {
     let start_time = time::Instant::now();
-    let _rnds = sym_standard_rands(0.7, 100_000_000).unwrap();
+    let _rnds = sym_standard_rands(0.7, 10_000_000).unwrap();
     let elapsed = start_time.elapsed().as_secs_f64();
     println!("Time taken by Rust: {elapsed} s");
 }
