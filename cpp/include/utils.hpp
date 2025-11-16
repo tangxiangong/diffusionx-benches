15fae57db60bd618910b187e51a1e767543773c0
tangxiangong <tangxiangong@gmail.com>
2025-11-16 14:46:51 +0800

    Refactor `utils.hpp` to support arbitrary moment calculations and apply clang-format
    Rename mc_msd to mc_moment with an order parameter to compute arbitrary statistical moments (mean, MSD, etc.). Update bm.cpp and langevin.cpp to use mc_moment with order=2 for MSD calculations. Apply clang-format across all files for consistent code style.

--- cpp/include/utils.hpp
+++ cpp/include/utils.hpp
@@ -1,106 +1,120 @@
 #ifndef DIFFUSIONX_BENCHES_UTILS_HPP
 #define DIFFUSIONX_BENCHES_UTILS_HPP
 
-#include <vector>
-#include <thread>
-#include <concepts>
-#include <chrono>
 #include <algorithm>
+#include <chrono>
+#include <concepts>
 #include <numeric>
 #include <print>
+#include <thread>
+#include <vector>
 
 using std::vector;
 
-template<typename T>
+template <typename T>
 concept Simulator = std::invocable<T> &&
-    std::same_as<std::invoke_result_t<T>, std::pair<vector<double>, vector<double>>>;
-
+                    std::same_as<std::invoke_result_t<T>,
+                                 std::pair<vector<double>, vector<double>>>;
 
 constexpr double NANOSECONDS_PER_SECOND = 1000000000.0;
 
-template<typename F>
-auto timeit(F func) -> double {
-    auto start_time = std::chrono::high_resolution_clock::now();
-    auto res = func();
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-    auto elapsed = static_cast<double>(duration.count()) / NANOSECONDS_PER_SECOND;
+template <typename F> auto timeit(F func) -> double {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  auto res = func();
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      end_time - start_time);
+  auto elapsed = static_cast<double>(duration.count()) / NANOSECONDS_PER_SECOND;
 
-    return elapsed;
+  return elapsed;
 }
 
 auto linspace(double start, double end, double step) -> vector<double> {
-    vector<double> result;
-    for (double val = start; val <= end; val += step) {
-        result.push_back(val);
-    }
-    if (std::abs(result.back()-end) > 1e-5) {
-        result.push_back(end);
-    } else {
-        result.back() = end;
-    }
-    return result;
+  vector<double> result;
+  for (double val = start; val <= end; val += step) {
+    result.push_back(val);
+  }
+  if (std::abs(result.back() - end) > 1e-5) {
+    result.push_back(end);
+  } else {
+    result.back() = end;
+  }
+  return result;
 }
 
-template<typename F>
-auto bench(F func, size_t bench_size) -> void {
-    vector<double> result(bench_size);
-    for (auto &val : result) {
-        auto start_time = std::chrono::high_resolution_clock::now();
-        auto res = func();
-        auto end_time = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-        auto elapsed = static_cast<double>(duration.count()) / NANOSECONDS_PER_SECOND;
-        val = elapsed;
-    }
-    double mean = std::accumulate(result.begin(), result.end(), 0.0) / result.size() * 1000.0;
-    double stddev = std::sqrt(std::accumulate(result.begin(), result.end(), 0.0, [mean](double acc, double x) {
-        return acc + ((x * 1000  - mean) * (x * 1000 - mean));
-    }) / result.size());
-    double min = *std::ranges::min_element(result) * 1000.0;
-    double max = *std::ranges::max_element(result) * 1000.0;
+template <typename F> auto bench(F func, size_t bench_size) -> void {
+  vector<double> result(bench_size);
+  for (auto &val : result) {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    auto res = func();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+        end_time - start_time);
+    auto elapsed =
+        static_cast<double>(duration.count()) / NANOSECONDS_PER_SECOND;
+    val = elapsed;
+  }
+  double mean = std::accumulate(result.begin(), result.end(), 0.0) /
+                result.size() * 1000.0;
+  double stddev = std::sqrt(std::accumulate(result.begin(), result.end(), 0.0,
+                                            [mean](double acc, double x) {
+                                              return acc + ((x * 1000 - mean) *
+                                                            (x * 1000 - mean));
+                                            }) /
+                            result.size());
+  double min = *std::ranges::min_element(result) * 1000.0;
+  double max = *std::ranges::max_element(result) * 1000.0;
 
-    std::println("mean: {:.4f} ms, stddev: {:.4f} , min: {:.4f} ms, max: {:.4f} ms", mean, stddev, min, max);
+  std::println(
+      "mean: {:.4f} ms, stddev: {:.4f} , min: {:.4f} ms, max: {:.4f} ms", mean,
+      stddev, min, max);
 }
 
-template<Simulator T>
-auto mc_msd(size_t particles, T simulate) -> double {
-    unsigned int num_threads = std::thread::hardware_concurrency();
-    vector<std::thread> threads;
-    threads.reserve(num_threads);
+template <Simulator T>
+auto mc_moment(size_t particles, int order, T simulate) -> double {
+  unsigned int num_threads = std::thread::hardware_concurrency();
+  vector<std::thread> threads;
+  threads.reserve(num_threads);
 
-    vector<double> partial_results(num_threads, 0.0);
+  vector<double> partial_results(num_threads, 0.0);
 
-    size_t chunk_size = particles / num_threads;
-    size_t remainder = particles % num_threads;
+  size_t chunk_size = particles / num_threads;
+  size_t remainder = particles % num_threads;
 
-    for(size_t i = 0; i < num_threads; ++i) {
-        size_t start = i*chunk_size;
-        size_t end = start + chunk_size;
-        if(i == num_threads - 1) {
-            end += remainder;
-        }
-        // Explicit capture - copy this to avoid shared pointer access
-        threads.emplace_back([i, start, end, &partial_results, &simulate]() -> void {
-            double local_sum = 0.0;
-            for(size_t j = start; j < end; ++j) {
-                auto [t, x] = simulate();
-                double dx = x.back() - x.front();
-                local_sum += (dx * dx);
+  for (size_t i = 0; i < num_threads; ++i) {
+    size_t start = i * chunk_size;
+    size_t end = start + chunk_size;
+    if (i == num_threads - 1) {
+      end += remainder;
+    }
+    // Explicit capture - copy this to avoid shared pointer access
+    threads.emplace_back(
+        [i, start, end, order, &partial_results, &simulate]() -> void {
+          double local_sum = 0.0;
+          for (size_t j = start; j < end; ++j) {
+            auto [t, x] = simulate();
+            double dx = x.back() - x.front();
+            double inc;
+            if (order == 1) {
+              inc = dx;
+            } else {
+              inc = std::pow(dx, order);
             }
-            partial_results[i] = local_sum;
+            local_sum += inc;
+          }
+          partial_results[i] = local_sum;
         });
-    }
+  }
 
-    for(auto& thread : threads) {
-        thread.join();
-    }
+  for (auto &thread : threads) {
+    thread.join();
+  }
 
-    double total_sum = 0.0;
-    for(double partial: partial_results) {
-        total_sum += partial;
-    }
-    return total_sum / static_cast<double>(particles);
+  double total_sum = 0.0;
+  for (double partial : partial_results) {
+    total_sum += partial;
+  }
+  return total_sum / static_cast<double>(particles);
 }
 
-#endif //DIFFUSIONX_BENCHES_UTILS_HPP
+#endif // DIFFUSIONX_BENCHES_UTILS_HPP
--- cpp/src/bm.cpp
+++ cpp/src/bm.cpp
@@ -1,51 +1,56 @@
-#include <utility>
-#include <vector>
-#include <print>
-#include <ranges>
 #include "random/normal.hpp"
 #include "utils.hpp"
+#include <print>
+#include <ranges>
+#include <utility>
+#include <vector>
 
 using std::vector;
 
 struct Bm {
-    double starting_position = 0.0;
-    double diffusivity = 0.5;
-    Bm() = default;
-    Bm(double starting_position, double diffusivity) : starting_position(starting_position), diffusivity(diffusivity) {}
+  double starting_position = 0.0;
+  double diffusivity = 0.5;
+  Bm() = default;
+  Bm(double starting_position, double diffusivity)
+      : starting_position(starting_position), diffusivity(diffusivity) {}
 
-    [[nodiscard]] auto simulate(double duration, double tau = 0.01) const -> std::pair<vector<double>, vector<double>> {
-        vector<double> t = linspace(0.0, duration, tau);
-        size_t n = t.size() - 1;
-        vector<double> x(n+1);
-        x[0] = starting_position;
-        double sigma = std::sqrt(2 * diffusivity * tau);
-        auto noise = randn(n).value();
-        for (size_t i = 0; i < n; ++i) {
-           x[i+1] = x[i] + (sigma * noise[i]);
-        }
-        return std::make_pair(t, x);
+  [[nodiscard]] auto simulate(double duration, double tau = 0.01) const
+      -> std::pair<vector<double>, vector<double>> {
+    vector<double> t = linspace(0.0, duration, tau);
+    size_t n = t.size() - 1;
+    vector<double> x(n + 1);
+    x[0] = starting_position;
+    double sigma = std::sqrt(2 * diffusivity * tau);
+    auto noise = randn(n).value();
+    for (size_t i = 0; i < n; ++i) {
+      x[i + 1] = x[i] + (sigma * noise[i]);
     }
+    return std::make_pair(t, x);
+  }
 
-    [[nodiscard]] auto msd(double duration, double tau=0.01, size_t particles=10000) const -> double {
-        auto simulator = [this, duration, tau]() -> std::pair<vector<double>, vector<double>> {
-            return this->simulate(duration, tau);
-        };
-        return mc_msd(particles, simulator);
-    }
+  [[nodiscard]] auto msd(double duration, double tau = 0.01,
+                         size_t particles = 10000) const -> double {
+    auto simulator = [this, duration,
+                      tau]() -> std::pair<vector<double>, vector<double>> {
+      return this->simulate(duration, tau);
+    };
+    return mc_moment(particles, 2, simulator);
+  }
 };
 
 int main() {
-    Bm bm {};
-    auto duration = std::views::iota(1, 11) | std::views::transform([](int v) -> double {
+  Bm bm{};
+  auto duration =
+      std::views::iota(1, 11) | std::views::transform([](int v) -> double {
         return static_cast<double>(v * 100);
-    });
-    auto func = [&]() -> int {
-        for(const double& d: duration) {
-            double m = bm.msd(d);
-        }
-        return 0;
-    };
-    auto elapsed = timeit(func);
-    std::println("Elapsed {}s", elapsed);
+      });
+  auto func = [&]() -> int {
+    for (const double &d : duration) {
+      double m = bm.msd(d);
+    }
     return 0;
+  };
+  auto elapsed = timeit(func);
+  std::println("Elapsed {}s", elapsed);
+  return 0;
 }
